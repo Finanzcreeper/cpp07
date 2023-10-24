d791f3ddbe27a6250c7367eb50bdba74a312d77b
Nico Reher <nreher@student.42berlin.de>
2023-10-24 15:59:26 +0200

    1

---
+++ ex00/main.cpp
@@ -0,0 +1,10 @@
+#include "templates.hpp"
+
+int main() {
+	int a = 12;
+	int b = 8;
+	::swap(a,b){
+
+	}
+
+}
---
+++ ex00/templates.hpp
@@ -0,0 +1,7 @@
+#ifndef CPP07_TEMPLATES_HPP
+#define CPP07_TEMPLATES_HPP
+
+template<typename input>
+void swap(input a, input b);
+
+#endif //CPP07_TEMPLATES_HPP
