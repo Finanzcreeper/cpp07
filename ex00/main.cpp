e028968628461112a6b6785aa026eb7d04d4eb42
Nico Reher <nreher@student.42berlin.de>
2023-10-24 16:57:16 +0200

    ex00 added

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,10 +1,43 @@
 #include "templates.hpp"
+#include <iostream>
+#include <string>
 
 int main() {
-	int a = 12;
-	int b = 8;
-	::swap(a,b){
+	{
+		int a = 12;
+		int b = 8;
+		int min;
+		int max;
 
+		std::cout << "a: " << a << "	b: " << b << std::endl;
+		::swap(a, b);
+		std::cout << "a: " << a << "	b: " << b << std::endl;
+		std::cout << "-----------------" << std::endl;
+		std::cout << "a: " << a << "	b: " << b << std::endl;
+		min = ::min(a, b);
+		std::cout << "min is: " << min << std::endl;
+		std::cout << "-----------------" << std::endl;
+		std::cout << "a: " << a << "	b: " << b << std::endl;
+		max = ::max(a, b);
+		std::cout << "max is: " << max << std::endl;
 	}
+	std::cout << "==============================================" << std::endl;
+	{
+		std::string a = "test";
+		std::string b = "bla";
+		std::string min;
+		std::string max;
 
+		std::cout << "a: " << a << "	b: " << b << std::endl;
+		::swap(a, b);
+		std::cout << "a: " << a << "	b: " << b << std::endl;
+		std::cout << "-----------------" << std::endl;
+		std::cout << "a: " << a << "	b: " << b << std::endl;
+		min = ::min(a, b);
+		std::cout << "min is: " << min << std::endl;
+		std::cout << "-----------------" << std::endl;
+		std::cout << "a: " << a << "	b: " << b << std::endl;
+		max = ::max(a, b);
+		std::cout << "max is: " << max << std::endl;
+	}
 }
--- ex00/templates.hpp
+++ ex00/templates.hpp
@@ -1,7 +1,27 @@
 #ifndef CPP07_TEMPLATES_HPP
 #define CPP07_TEMPLATES_HPP
 
+#include <iostream>
+
 template<typename input>
-void swap(input a, input b);
+void swap(input &a, input &b){
+	input temp = a;
+	a = b;
+	b = temp;
+}
+
+template<typename var>
+var max(var a, var b) {
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+template<typename var>
+var min(var a, var b) {
+	if (a < b)
+		return (a);
+	return (b);
+}
 
 #endif //CPP07_TEMPLATES_HPP
